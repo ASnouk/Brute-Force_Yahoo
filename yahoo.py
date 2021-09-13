caf719ad53bc8d223676074b5123c795566acc25
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-13 09:36:50 -0300

    Update yahoo.py

--- yahoo.py
+++ yahoo.py
@@ -57,7 +57,7 @@ if do == '1':
     senha = input("passlist : ")
     headers = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-    instance.accounts.append(user)
+    instance.accounts.append(username)
     instance.get_pass_list(senha)
 
     instance.try_yahoo()
